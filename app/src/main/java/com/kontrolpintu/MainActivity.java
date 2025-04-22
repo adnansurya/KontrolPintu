0d7a035124e05a0ea0d4df2783923598c5a59fc8
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-04-22 14:18:12 +0800

    last update

--- app/src/main/java/com/kontrolpintu/MainActivity.java
+++ app/src/main/java/com/kontrolpintu/MainActivity.java
@@ -89,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
         btnSpeech.setOnClickListener(view -> startListening());
     }
 
+
+
     private void startListening() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
